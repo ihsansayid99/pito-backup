6ac0066570ba2fafc327affad2c2713dbfc65b99
Ihsan Sayid Muharram <61352696+ihsansayid99@users.noreply.github.com>
2020-10-20 19:34:37 +0700

    adding analytic

--- src/pages/analytic.js
+++ src/pages/analytic.js
@@ -3,7 +3,7 @@ import Sidebar from 'components/SideNavbar'
 
 const Analytic = () => {
     useEffect(() => {
-        document.getElementById("embeded-dashboard").src = "https://embed.app.octoboard.com/embed?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImF2djlXODd5d2l4bmhlY2luLWNrZ2h3ZjBhbjBzbW8wN3JuYnY3MWd4YmdAb2N0b2JvYXJkLmNvbSIsInBhc3N3b3JkIjoiOTQ4YTRiNzQyMGQ1NjNhYmVmZDk5MzMwODQ1NjY3M2MzZWNmMjE0MGRlYTMwMTQ5YTE5MGRhYTM2NGI2ZjVmMSIsImlhdCI6MTYwMzE5NDE3Mn0.KY2wDNChsdEW78MZSIAjS5LSy466ZoqPa3WXugI-P1k"
+        document.getElementById("embeded-dashboard").src = "https://embed.app.octoboard.com/embed?token=https://embed.app.octoboard.com/embed?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImF2djlXODd5d2l4bmhlY2luLWNrZ2h3ZjBhbjBzbW8wN3JuYnY3MWd4YmdAb2N0b2JvYXJkLmNvbSIsInBhc3N3b3JkIjoiOTQ4YTRiNzQyMGQ1NjNhYmVmZDk5MzMwODQ1NjY3M2MzZWNmMjE0MGRlYTMwMTQ5YTE5MGRhYTM2NGI2ZjVmMSIsImlhdCI6MTYwMzE5NzE5OH0.frqPI7zSGWk6DSdJSJhu_OzpResHvtlu84Hk-Az_DQ8"
     })
     return (
         <>
@@ -12,7 +12,6 @@ const Analytic = () => {
                 <div className="pt-20 px-5 w-full flex">
                     <iframe title="Dashboard Analytic" id="embeded-dashboard" allowtransparency="true" scrolling="no" frameBorder="0" style={{ border: "none", overflow: "hidden", minHeight: "600px", width: "100%" }}></iframe>
                 </div>
-
             </section>
         </>
     )
